0e8c2e2523083c0cc2322acd3acce98585110b7a
khanhvx2005 <kvuxuan789@gmail.com>
2026-04-15 11:27:32 +0700

    hiển thị data section 2

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,20 @@
-module.exports.home = (req, res) => {
-    res.render('client/pages/home.pug', { titlePage: "Trang chủ" })
+const Tour = require("../../models/tour.model")
+const moment = require('moment')
+module.exports.home = async (req, res) => {
+    // Section 2
+    const tourListSection2 = await Tour
+        .find({
+            deleted: false,
+            status: "active"
+        })
+        .limit(3)
+        .sort({
+            position: 'desc'
+        })
+    for (const item of tourListSection2) {
+        item.discount = (((item.priceAdult - item.priceNewAdult) / item.priceAdult) * 100).toFixed();
+        item.departureDateFormat = moment(item.departureDate).format('DD/MM/YYYY');
+    }
+    // End Section 2
+    res.render('client/pages/home.pug', { titlePage: "Trang chủ", tourListSection2: tourListSection2 })
 }
