4bdf3042a27eaf7d9f38223240259be04eea6a26
khanhvx2005 <kvuxuan789@gmail.com>
2026-04-22 21:36:47 +0700

    Quản lý đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,12 +1,32 @@
+const Order = require("../../models/order.model")
+const variableConfig = require('../../config/variable')
+const moment = require("moment")
 module.exports.list = async (req, res) => {
-
-    res.render("admin/pages/order-list", {
-        pageTitle: "Quản lý đơn hàng",
+  const find = {
+    deleted: false
+  }
+  const orderList = await Order
+    .find(find)
+    .sort({
+      createdAt: 'desc'
     })
+  for (const orderDetail of orderList) {
+    orderDetail.paymentMethodName = variableConfig.paymentMethod.find(item => item.value == orderDetail.paymentMethod).label;
+    orderDetail.paymentStatusName = variableConfig.paymentStatus.find(item => item.value == orderDetail.paymentStatus).label;
+    orderDetail.statusName = variableConfig.status.find(item => item.value == orderDetail.status).label;
+
+    orderDetail.createAtTime = moment(orderDetail.createdAt).format("HH:mm")
+    orderDetail.createAtDate = moment(orderDetail.createdAt).format("DD/MM/YYYY")
+
+  }
+  res.render("admin/pages/order-list", {
+    pageTitle: "Quản lý đơn hàng",
+    orderList: orderList
+  })
 }
 module.exports.edit = async (req, res) => {
 
-    res.render("admin/pages/order-edit", {
-        pageTitle: "Chỉnh sửa đơn hàng",
-    })
+  res.render("admin/pages/order-edit", {
+    pageTitle: "Chỉnh sửa đơn hàng",
+  })
 }
