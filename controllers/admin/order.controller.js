2c0ee25b116ef60cf7b4d5148b4f5f6d83ce18aa
khanhvx2005 <kvuxuan789@gmail.com>
2026-04-23 11:10:42 +0700

    cập nhập đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,6 +1,7 @@
 const Order = require("../../models/order.model")
 const variableConfig = require('../../config/variable')
 const moment = require("moment")
+const City = require("../../models/city.model")
 module.exports.list = async (req, res) => {
   const find = {
     deleted: false
@@ -25,8 +26,58 @@ module.exports.list = async (req, res) => {
   })
 }
 module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const orderDetail = await Order.findOne({
+      _id: id,
+      deleted: false
+    })
+    if (!orderDetail) {
+      res.redirect(`/${pathAdmin}/order/list`)
+      return;
+    }
+    orderDetail.createdAtFormat = moment(orderDetail.createdAt).format("YYYY-MM-DDTHH:mm")
+    for (const item of orderDetail.items) {
+      item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY")
+      const city = await City.findOne({
+        _id: item.locationFrom
+      })
+      if (city) {
+        item.locationFromName = city.name;
 
-  res.render("admin/pages/order-edit", {
-    pageTitle: "Chỉnh sửa đơn hàng",
-  })
+      }
+    }
+    // console.log(orderDetail)
+    res.render("admin/pages/order-edit", {
+      pageTitle: `Chỉnh sửa đơn hàng ${orderDetail.orderCode}`,
+      orderDetail: orderDetail,
+      paymentMethod: variableConfig.paymentMethod,
+      paymentStatus: variableConfig.paymentStatus,
+      status: variableConfig.status
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect(`/${pathAdmin}/order/list`)
+  }
+
+}
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    req.body.updatedBy = req.account.id;
+    await Order.updateOne({
+      _id: id
+    }, req.body)
+    req.flash("success", "Cập nhập thành công")
+    res.json({
+      code: "success",
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({
+      code: "error",
+      message: "Thông tin đơn hàng không hợp lệ"
+    })
+  }
+
 }
