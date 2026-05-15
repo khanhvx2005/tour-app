159d003f7dd65edf9fcee50afb72ddbe185a2889
khanhvx2005 <kvuxuan789@gmail.com>
2026-05-15 22:50:49 +0700

    fix check ma giam gia

--- controllers/client/coupon.controller.js
+++ controllers/client/coupon.controller.js
@@ -1,23 +1,47 @@
 const Coupon = require("../../models/coupon.model");
 
 module.exports.detail = async (req, res) => {
-  const { coupon } = req.body;
-  const exitsCoupon = await Coupon.findOne({
-    code: coupon,
-    deleted: false,
-    status: "active"
-  }).select('code discountValue maxDiscountAmount name quantity')
-  if (!exitsCoupon) {
+  try {
+    const { coupon } = req.body;
+    const existsCoupon = await Coupon.findOne({
+      code: coupon,
+      deleted: false,
+      status: "active"
+    })
+    if (!existsCoupon) {
+      res.json({
+        code: "error",
+        message: "Mã giảm giá không tồn tại hoặc chưa được kích hoạt!"
+      })
+      return;
+    }
+    if (existsCoupon.quantity <= 0) {
+      res.json({
+        code: "error",
+        message: "Mã giảm giá này đã hết lượt sử dụng"
+      })
+      return;
+    }
+    const currentTime = new Date();
+    // Nếu thời gian hiện tại lớn hơn ngày kết thúc -> Hết hạn
+    if (existsCoupon && existsCoupon.endDate && currentTime > existsCoupon.endDate) {
+      res.json({
+        code: "error",
+        message: "Mã giảm giá này đã hết hạn chương trình."
+      })
+      return;
+    }
+
+    res.json({
+      code: "success",
+      message: "Thành công!",
+      couponDetail: existsCoupon
+    })
+  } catch (error) {
+    console.error("Lỗi khi kiểm tra mã giảm giá:", error);
     res.json({
       code: "error",
-      message: "Mã giảm giá không hợp lệ!"
+      message: "Lỗi máy chủ nội bộ. Vui lòng thử lại sau!",
     })
-    return;
   }
-
-  res.json({
-    code: "success",
-    message: "Thành công!",
-    couponDetail: exitsCoupon
-  })
 }
